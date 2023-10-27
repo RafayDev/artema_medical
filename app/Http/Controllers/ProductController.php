aab686d5ea0371c7dc445eb4106d6da99caab13b
RafayDev <imabdulrafayy@gmail.com>
2023-10-27 10:50:33 -0700

    auth

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,6 +10,21 @@ use App\Models\ProductSize;
 
 class ProductController extends Controller
 {
+        /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
     public function index()
     {
         $categories=Category::all();
